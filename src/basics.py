8f03b3ac16eb031ef9de38a58e6ab2f49c380998
Gurmehar Singh <gurmehar@gmail.com>
2023-08-30 15:56:08 -0400

    add basics.py

---
+++ src/basics.py
@@ -0,0 +1,41 @@
+# TO-DO: 
+
+G_CONST = 6.67e-11 # N * m^2 / kg^2
+
+M_SUN = 2e30 # kg
+M_EARTH = 6e24 # kg
+M_SATURN_V_ROCKET = 3e6 # kg
+
+R_EARTH = 6400 # km
+
+D_SUN_EARTH = 150e6 # km
+
+#*-------------------------*#
+#*-----TO BE CALCULATED----*#
+#*-------------------------*#
+F_SUN_EARTH = None
+F_ROCKET_EARTH = None
+EARTH_ESC_VEL = None
+#*-------------------------*#
+#*-------------------------*#
+#*-------------------------*#
+
+#*-------------------------*#
+#*----DO YOUR WORK HERE----*#
+#*-------------------------*#
+
+
+
+
+#*-------------------------*#
+#*-------------------------*#
+#*-------------------------*#
+
+
+
+print("The gravitational force between the Earth and the Sun is %.2f Newtons.")
+print("The gravitational force between the Earth and the Saturn V Rocket is %.2f Newtons.")
+print("The escape velocity on Earth is %.2f m/s.")
+
+
+
